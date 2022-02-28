ea152bb5312a7da79eecdac98aade9b3efa750c6
joaociasul <joaocarlosroyer2018@gmail.com>
2022-02-28 12:42:04 -0300

    auth

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,6 +5,16 @@ const {
 } = require('../middlwares/auth');
 const app = express();
 
+
+app.get('/teste', auth, (req, res) => {
+    res.status(200).send(req.user)
+})
+app.get('/set', (req, res) => {
+    res.send({
+        token: setToken(5)
+    })
+})
+
 const urlCompany = '/company'
 app.post(urlCompany, auth, CompanyController.createAcion)
 app.get(urlCompany + '/getById/:_id?', auth, CompanyController.index) //to filter, use req.query
